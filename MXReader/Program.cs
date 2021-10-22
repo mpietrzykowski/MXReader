bc15ab22f53e6de6f020bb6a6aa3fd7cecb1b0e3
Marcin Pietrzykowski <marcin.pietrzykowski.1985@gmail.com>
2021-10-22 16:10:13 +0200

    first

---
+++ MXReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MXReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
